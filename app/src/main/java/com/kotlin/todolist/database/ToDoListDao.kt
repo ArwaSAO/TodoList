3f79d5fb5c2cdc29fb9e11877f7665713df7e05b
= <=>
2021-10-27 16:33:39 +0300

    I did some design changes in the fragments and now  I am starting to build the database and Dao

--- app/src/main/java/com/kotlin/todolist/database/ToDoListDao.kt
+++ app/src/main/java/com/kotlin/todolist/database/ToDoListDao.kt
@@ -1,4 +1,21 @@
 package com.kotlin.todolist.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.kotlin.todolist.database.model.ToDoListItemModel
+
+
+@Dao
 interface ToDoListDao {
+
+
+
+
+    @Insert
+    suspend fun addItem(toDoListItemModel: ToDoListItemModel)
+
+    @Query("SELECT * FROM todolistitemmodel")
+    fun getItems():LiveData<List<ToDoListItemModel>>
 }
--- app/src/main/java/com/kotlin/todolist/database/ToDoListDataBase.kt
+++ app/src/main/java/com/kotlin/todolist/database/ToDoListDataBase.kt
@@ -1,4 +1,12 @@
 package com.kotlin.todolist.database
 
-class ToDoListDataBase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.kotlin.todolist.database.model.ToDoListItemModel
+
+
+
+@Database(entities = [ToDoListItemModel::class], version = 1)
+abstract class ToDoListDataBase: RoomDatabase() {
+    abstract fun toDoListDao():ToDoListDao
 }
